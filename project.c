cb20f811bd5920b219bbc8c1fc291b0eea1c3a14
lucianhepcal <163905542+lucianhepcal@users.noreply.github.com>
2024-05-11 14:49:06 +0300

    Update project.c

--- project.c
+++ project.c
@@ -1,97 +1,150 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <fcntl.h>
 #include <time.h>
+#include <stdbool.h>
+#include <sys/wait.h>
 
-struct FileSnapshot {
-    char name[256];
-    int size;
-    char location[300];
-    ino_t inode;
-    time_t lastModified;
+#define PATH_MAX 4096
+#define MAX_ENTRIES 100
+
+struct FileMetadata {
+    char entry_name[PATH_MAX];
+    time_t last_modified;
+    off_t size;
+    mode_t permissions;
 };
 
-void generateFileSnapshots(DIR *dirp, char *path, int level, struct FileSnapshot *snapshots) {
-    struct dirent *dp;
-    struct stat buf;
-    char *newPath = (char *)malloc(5000);
-    
-    while ((dp = readdir(dirp)) != NULL) {
-        if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0) {
-            continue; 
-        }
-        strcpy(newPath, path);
-        strcat(newPath, dp->d_name);
-        stat(newPath, &buf);
-        strcpy(snapshots[level].name, dp->d_name);
-        snapshots[level].size = buf.st_size;
-        strcpy(snapshots[level].location, newPath);
-        snapshots[level].inode = buf.st_ino;
-        snapshots[level].lastModified = buf.st_mtime;
+void update_snapshot(const char *dir_path, struct FileMetadata *old_snapshot, const char *isolated_space) {
+    printf("Updating snapshot for directory: %s\n", dir_path);
 
-        level++;
-        if (S_ISDIR(buf.st_mode)) {
-            strcat(newPath, "/");
-            DIR *newDir = opendir(newPath);
-            generateFileSnapshots(newDir, newPath, level, snapshots);
-            closedir(newDir);
+    char snapshot_path[PATH_MAX];
+    snprintf(snapshot_path, PATH_MAX, "%s/Snapshot.txt", dir_path);
+
+    int snapshot_fd = open(snapshot_path, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (snapshot_fd == -1) {
+        perror("Failed to open snapshot file");
+        exit(EXIT_FAILURE);
+    }
+
+    dprintf(snapshot_fd, "Snapshot of directory: %s\n", dir_path);
+    dprintf(snapshot_fd, "---------------------------------\n");
+
+    DIR *dir;
+    struct dirent *entry;
+    dir = opendir(dir_path);
+    if (dir == NULL) {
+        perror("Error opening directory");
+        exit(EXIT_FAILURE);
+    }
+
+    //struct FileMetadata new_snapshot[MAX_ENTRIES];
+    int num_entries = 0;
+
+    while ((entry = readdir(dir)) != NULL && num_entries < MAX_ENTRIES) {
+
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+            struct stat st;
+            char entry_path[PATH_MAX];
+            snprintf(entry_path, PATH_MAX, "%s/%s", dir_path, entry->d_name);
+
+            if (stat(entry_path, &st) == -1) {
+                perror("Error getting file status");
+                exit(EXIT_FAILURE);
+            }
+
+            if(strcmp(entry->d_name, "Snapshot.txt") == 0) continue;
+            struct FileMetadata metadata;
+            strcpy(metadata.entry_name, entry->d_name);
+            metadata.last_modified = st.st_mtime;
+            metadata.size = st.st_size;
+            int Pipe[2];
+            pipe(Pipe);
+            pid_t dpid = fork();
+            if(dpid == -1){
+                perror("fork faile");
+                exit(EXIT_FAILURE);
+            } else if(dpid == 0) {
+                close(Pipe[0]);
+
+                if((S_ISREG(st.st_mode) || S_ISDIR(st.st_mode)) && stat(entry_path, &st) != -1 && ((st.st_mode & S_IRUSR) || (st.st_mode & S_IWUSR) || (st.st_mode & S_IXUSR))){
+                    
+                    metadata.permissions = st.st_mode;
+                } else {
+                    metadata.permissions = 0;
+                }
+
+                write(Pipe[1], &metadata.permissions, sizeof(mode_t));
+                
+                close(Pipe[1]);
+                exit(EXIT_SUCCESS);
+            } else {
+                close(Pipe[1]);
+
+                mode_t child_permissions;
+                read(Pipe[0], &child_permissions, sizeof(mode_t));
+                metadata.permissions = child_permissions;
+                close(Pipe[0]);
+            }
+            
+            
+
+            //new_snapshot[num_entries++] = metadata;
+            
+            if(metadata.permissions == 0){
+                char newPath[PATH_MAX];
+                snprintf(newPath, PATH_MAX, "%s/%s", isolated_space, entry->d_name);
+                rename(entry_path, newPath);
+            }
+
+            num_entries++;
+            dprintf(snapshot_fd, "Entry: %s\n", metadata.entry_name);
+            dprintf(snapshot_fd, "Size: %lld bytes\n", (long long)metadata.size);
+            dprintf(snapshot_fd, "Permissions: %o\n", metadata.permissions);
+            dprintf(snapshot_fd, "Last Modified: %s\n", ctime(&metadata.last_modified));
+            dprintf(snapshot_fd, "\n");
+
+            // Recurse into subdirectories
+            if (S_ISDIR(st.st_mode)) {
+                update_snapshot(entry_path, old_snapshot, isolated_space);
+            }
         }
     }
 
-    free(newPath);
+    closedir(dir);
+    close(snapshot_fd);
 }
 
-void createSnapshots(int argc, char **argv) {
-    struct FileSnapshot snapshots[1000];
-    for (int i = 0; i < 1000; i++) {
-        snapshots[i].size = -1;
+int main(int argc, char *argv[]) {
+    if (argc < 3 || argc > 12) {
+        printf("Usage: %s -s isolated_space_dir dir1 [dir2 ... dir10]\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
-    for (int i = 1; i < argc; i++) {
+
+    // Initial update of snapshots
+    for (int i = 2; i < argc; i++) {
         pid_t pid = fork();
         if (pid == -1) {
-            perror("fork error");
-            exit(1);
-        } else if (pid == 0) { 
-            DIR *dirp = opendir(argv[i]);
-            if (dirp == NULL) {
-                perror("opendir error");
-                exit(1);
-            }
-
-            generateFileSnapshots(dirp, argv[i], 0, snapshots);
-            closedir(dirp);
-            char snapLocation[5000];
-            strcpy(snapLocation, argv[i]);
-            strcat(snapLocation, "snapshot.txt");
-            FILE *filePtr = fopen(snapLocation, "w");
-            if (filePtr == NULL) {
-                perror("fopen error");
-                exit(1);
-            }
-            for (int j = 0; j < 1000; j++) {
-                fprintf(filePtr, "%s %d %s %ld %ld\n", snapshots[j].name, snapshots[j].size, 
-                        snapshots[j].location, snapshots[j].inode, snapshots[j].lastModified);
-            }
-            fclose(filePtr);
-            exit(0);
+            perror("fork failed");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) { // Child process
+            // Execute update_snapshot for the directory*/
+            update_snapshot(argv[i], NULL, argv[1]);
+            exit(EXIT_SUCCESS);
         }
     }
-    for (int i = 1; i < argc; i++) {
-        wait(NULL);
-    }
-}
 
-int main(int argc , char **argv) {//TODO debug cu numar mai mare de foldere/fisiere
-    if (argc < 2) {
-        printf("Usage: %s <directory1> <directory2> ...\n", argv[0]);
-        return 0;
+    // Wait for all child processes to finish
+    int status;
+    pid_t wpid;
+    while ((wpid = wait(&status)) > 0) {
+        printf("The child process with PID %d has ended with code %d.\n", wpid, WEXITSTATUS(status));
     }
-    createSnapshots(argc, argv);
+
     return 0;
 }
